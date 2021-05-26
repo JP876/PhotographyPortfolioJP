cecbdcdead689a508660a5fbe13935e5064a8c3b
Josip <popovicjosip867@gmail.com>
2021-05-26 20:44:33 +0200

    Small changes

--- client/src/AboutMe/Aboutme.js
+++ client/src/AboutMe/Aboutme.js
@@ -87,9 +87,7 @@ const Aboutme = () => {
                 <Typography variant='h6'>
                     Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                     accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-                    ab illo inventore veritatis et quasi architecto beatae vitae dicta
-                    sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-                    aspernatur aut odit.
+                    ab illo inventore veritatis et.
                 </Typography>
             </Paper>
         </motion.div>
